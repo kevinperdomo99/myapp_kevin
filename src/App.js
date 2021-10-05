d6f120e28b2d7a2d41ce889f49adec8c7954d1d7
AngelGalindez <agalindezbrazon@gmail.com>
2021-10-05 15:57:52 -0500

    cambiamos el color del boton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor (props)  {
+     super(props)
+
+    this.state={
+        color:"pink"
+    }
+    this.cambiarColor= this.cambiarColor.bind(this);
+
+    }
+    
+    cambiarColor(){
+      if(this.state.color == "pink"){
+        this.setState({color:"orange"})
+      }else{
+        this.setState({color:"pink"})
+      }
+     
+    }
+
+  
+
+
+
+
+
+  render(){ 
+    return(
+      <div className="App">
+        <button onClick={this.cambiarColor} style={{backgroundColor: this.state.color}}>
+          Click Aquí  
+        </button> 
+
+
+      </div>
+    )
+    
+    
+  }  
 }
 
 export default App;
